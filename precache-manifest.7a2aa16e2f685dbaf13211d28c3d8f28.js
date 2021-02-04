544e10dbcc3fa4d09ec01b7edf805a07e8da60e9
alicereactus <agamia@mail.ru>
2021-02-04 16:35:36 +0300

    Updates

---
+++ precache-manifest.7a2aa16e2f685dbaf13211d28c3d8f28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "652534b59a651588f68075a337d8e453",
+    "url": "/project-currency-exchange/index.html"
+  },
+  {
+    "revision": "53d972897fc93a110b9c",
+    "url": "/project-currency-exchange/static/css/main.33921b11.chunk.css"
+  },
+  {
+    "revision": "bf3c0f2a71846b69657e",
+    "url": "/project-currency-exchange/static/js/2.f7bbef87.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-currency-exchange/static/js/2.f7bbef87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53d972897fc93a110b9c",
+    "url": "/project-currency-exchange/static/js/main.72b9e550.chunk.js"
+  },
+  {
+    "revision": "61f68990c72fa3225e3a",
+    "url": "/project-currency-exchange/static/js/runtime-main.f2b93ab1.js"
+  }
+]);
